3255e3881811d87bd83dd6906c4e25278450f997
Sarun Wongtanakarn <2062638+mapfap@users.noreply.github.com>
2019-10-02 12:56:24 +0700

    Implemented Room CRUD

--- src/db.js
+++ src/db.js
@@ -1,16 +1,65 @@
-const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
+const MongoClient = require('mongodb').MongoClient
+const assert = require('assert')
 
-const uri = process.env.MONGO;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const uri = process.env.MONGO
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+let isConnected = false
+
 client.connect(err => {
-  const collection = client.db("test").collection("devices");
+  if (err) { 
+    console.log(err)
+    process.exit(-1)
+  }
 
-  collection.find({}).toArray(function(err, docs) {
-    assert.equal(err, null);
-    console.log("Found the following records");
-    console.log(docs)
-  });
+  isConnected = true
+  console.log(`[sys] Connected to MongoDB`)
+})
 
-  client.close();
-});
+const getRoomsPromise = () => {
+  return new Promise((resolve, reject) => {
+    client.db("test").collection("rooms").find({}).toArray(function(err, docs) {
+      err ? reject(err) : resolve(docs)
+    })
+  })
+}
+
+const createRoomPromise = (room) => {
+  return new Promise((resolve, reject) => {
+    client.db("test").collection("rooms").insertMany([room], function(err, docs) {
+      err ? reject(err) : resolve(docs)
+    })
+  })
+}
+
+const updateRoomPromise = (id, room) => {
+  return new Promise((resolve, reject) => {
+    client.db("test").collection("rooms")
+    .updateOne({ id : id }, { $set: room }, function(err, docs) {
+      err ? reject(err) : resolve(docs)
+    })
+  })
+}
+
+const getRooms = async () => {
+  assertDBConnection()
+  return await getRoomsPromise()
+}
+
+const createRoom = async (room) => {
+  assertDBConnection()
+  await createRoomPromise(room)
+}
+
+const updateRoom = async (id, room) => {
+  assertDBConnection()
+  await updateRoomPromise(id, room)
+}
+
+const assertDBConnection = () => {
+  if (!isConnected) throw new Error('DB is net connected')
+}
+
+module.exports = { getRooms, createRoom, updateRoom }
+
+
+
--- src/index.js
+++ src/index.js
@@ -1,12 +1,54 @@
 const express = require('express')
+const cors = require('cors')
+require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 8080
 const db = require('./db')
 
+app.use(express.json())
+app.use(cors())
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.json({up: true})
 })
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.get('/api/rooms', async (req, res) => {
+  try {
+    const rooms = await db.getRooms()
+    res.json(rooms.map(r => { 
+      return {id: r.id, isOccupied: r.isOccupied}
+    }))
+  } catch (err) {
+    console.log('GET /api/rooms', err)
+    res.status(500).json(err.message)
+  }
+})
+
+app.post('/api/rooms/', async (req, res) => {
+  try {
+    await db.createRoom({
+      id: req.body.id,
+      isOccupied: req.body.isOccupied
+    })
+    res.send(201)
+  } catch (err) {
+    console.log('POST /api/rooms', err)
+    res.status(500).json(err.message)
+  }
+})
+
+app.put('/api/rooms/:id', async (req, res) => {
+  try {
+    await db.updateRoom(req.params.id, {
+      isOccupied: req.body.isOccupied
+    })
+    res.send(200)
+  } catch (err) {
+    console.log('PUT /api/rooms', err)
+    res.status(500).json(err.message)
+  }
+})
+
+app.listen(port, () => console.log(`[sys] Express started on ${port}!`))
 
 module.exports = app
